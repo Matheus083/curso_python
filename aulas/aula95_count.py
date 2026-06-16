2e777f1c57d452c14e725d47cb8751a6fb51f412
Matheus083 <matheus2007n@gmail.com>
2026-06-16 20:42:20 -0300

    Feat: Exercício sobre soomar listas.

---
+++ aulas/aula95_count.py
@@ -0,0 +1,15 @@
+# count é um contador infinito sem fim.
+from itertools import count
+
+c1 = count()
+r1 = range(10)
+
+print('c1:', hasattr(c1, '__iter__'))
+print('c1:', hasattr(c1, '__next__'))
+print('r1:', hasattr(r1, '__iter__'))
+print('r1:', hasattr(r1, '__next__'))
+
+for i in c1:
+    print(i)
+
+    
